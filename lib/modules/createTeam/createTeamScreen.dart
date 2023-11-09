5c3e1d85c5963f06c6fb7f9888db80c5ada80a88
Aakash <99490497+dsaakash@users.noreply.github.com>
2023-11-09 18:21:52 +0530

    created team added functionality

--- lib/modules/createTeam/createTeamScreen.dart
+++ lib/modules/createTeam/createTeamScreen.dart
@@ -3,6 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CreateTeamScreen(),
+    );
+  }
+}
+
 class CreateTeamScreen extends StatefulWidget {
   @override
   _CreateTeamScreenState createState() => _CreateTeamScreenState();
@@ -25,6 +36,7 @@ class _CreateTeamScreenState extends State<CreateTeamScreen> {
   String time = '';
 
   Set<Map<String, dynamic>> selectedPlayers = Set<Map<String, dynamic>>();
+  Map<String, bool> playerPlayingStatus = {};
 
   @override
   void initState() {
@@ -38,14 +50,14 @@ class _CreateTeamScreenState extends State<CreateTeamScreen> {
     String? matchId = prefs.getString('matchId');
 
     country1Flag = prefs.getString('country1Flag') ?? '';
-    country1Name = prefs.getString('country1Name') ?? '';
+    country1Name = prefs.getString('country1Name')?.toUpperCase() ?? '';
     country2Flag = prefs.getString('country2Flag') ?? '';
-    country2Name = prefs.getString('country2Name') ?? '';
+    country2Name = prefs.getString('country2Name')?.toUpperCase() ?? '';
     time = prefs.getString('time') ?? '';
 
     if (cid != null && matchId != null) {
       final List<Map<String, dynamic>> players = await getPlayers(cid, matchId);
-      final response = await getMatchDetails(cid, matchId);
+      final response = await getMatchDetails(matchId);
 
       setState(() {
         allPlayerList = players;
@@ -67,13 +79,19 @@ class _CreateTeamScreenState extends State<CreateTeamScreen> {
         if (playing11Response != null) {
           setState(() {
             playing11 = playing11Response['players'] ?? [];
+
+            // Update playing status for each player
+            for (var player in allPlayerList) {
+              final isPlaying = playing11.contains(player);
+              playerPlayingStatus[player['short_name']] = isPlaying;
+            }
           });
         }
       }
     }
   }
 
-  Future<List<Map<String, dynamic>>> getPlayers(cid, matchId) async {
+  Future<List<Map<String, dynamic>>> getPlayers(String cid, String matchId) async {
     final List<Map<String, dynamic>> players = [];
     final String query = "https://rest.entitysport.com/v2/competitions/$cid/squads/$matchId?token=4c5b78057cd282704f2a9dd8ea556ee2";
     final Map<String, dynamic> headers = {"Content-Type": "application/json"};
@@ -104,7 +122,7 @@ class _CreateTeamScreenState extends State<CreateTeamScreen> {
     }
   }
 
-  Future<Map<String, dynamic>?> getMatchDetails(cid, matchId) async {
+  Future<Map<String, dynamic>?> getMatchDetails(String matchId) async {
     final String query = "https://rest.entitysport.com/v2/match/squads/$matchId?token=4c5b78057cd282704f2a9dd8ea556ee2";
     final Map<String, dynamic> headers = {"Content-Type": "application/json"};
 
@@ -127,7 +145,7 @@ class _CreateTeamScreenState extends State<CreateTeamScreen> {
     }
   }
 
-  Future<Map<String, dynamic>?> getPlaying11(matchId) async {
+  Future<Map<String, dynamic>?> getPlaying11(String matchId) async {
     final String query = "https://rest.entitysport.com/v2/matches/$matchId/squads?token=4c5b78057cd282704f2a9dd8ea556ee2";
     final Map<String, dynamic> headers = {"Content-Type": "application/json"};
 
@@ -162,171 +180,180 @@ class _CreateTeamScreenState extends State<CreateTeamScreen> {
     });
   }
 
- @override
-Widget build(BuildContext context) {
-  return DefaultTabController(
-    length: 4,
-    child: Scaffold(
-      appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(country1Name, style: TextStyle(fontSize: 14)),
+                  Text(country2Name, style: TextStyle(fontSize: 14)),
+                  Text(time, style: TextStyle(fontSize: 12)),
+                ],
+              ),
+              Image.network(
+                country1Flag,
+                height: 40,
+                width: 40,
+              ),
+              Image.network(
+                country2Flag,
+                height: 40,
+                width: 40,
+              ),
+            ],
+          ),
+        ),
+        body: ModalProgressHUD(
+          inAsyncCall: isDataLoading,
+          child: Column(
+            children: [
+              Expanded(
+                child: isDataLoading
+                  ? Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : DefaultTabController(
+                      length: 4,
+                      child: Column(
+                        children: [
+                          TabBar(
+                            labelColor: Colors.black,
+                            unselectedLabelColor: Colors.grey,
+                            tabs: [
+                              Tab(text: "WK (${wkCount.toString()})"),
+                              Tab(text: "BAT (${batCount.toString()})"),
+                              Tab(text: "BOWL (${bowlCount.toString()})"),
+                              Tab(text: "AR (${arCount.toString()})"),
+                            ],
+                          ),
+                          Expanded(
+                            child: TabBarView(
+                              children: [
+                                playerListView("wk", "Pick 1 Wicket-keeper"),
+                                playerListView("bat", "Pick 3-5 Batsmen"),
+                                playerListView("bowl", "Pick 3-5 Bowlers"),
+                                playerListView("all", "Pick 1-3 All-rounders"),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+              ),
+              if (selectedPlayers.length == 11)
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => PreviewScreen(selectedPlayers: selectedPlayers),
+                      ),
+                    );
+                  },
+                  child: Text("Preview"),
+                ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  // Continue button logic here
+                },
+                child: Text("Continue"),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.resolveWith<Color>((states) {
+                    if (selectedPlayers.length == 11) {
+                      return Colors.blue; // Enable when 11 players are selected
+                    }
+                    return Colors.grey; // Disable when not all players are selected
+                  }),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget playerListView(String role, String header) {
+    List<Map<String, dynamic>> filteredPlayers = allPlayerList.where((player) {
+      return (player['playing_role'] ?? '').toLowerCase() == role.toLowerCase();
+    }).toList();
+
+    return filteredPlayers.isEmpty
+        ? Center(
+            child: Text("No $role players available."),
+          )
+        : Expanded(
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(country1Name, style: TextStyle(fontSize: 14)),
-                Text(country2Name, style: TextStyle(fontSize: 14)),
-                Text(time, style: TextStyle(fontSize: 12)),
-              ],
-            ),
-            Image.network(
-              country1Flag,
-              height: 40,
-              width: 40,
-            ),
-            Image.network(
-              country2Flag,
-              height: 40,
-              width: 40,
-            ),
-          ],
-        ),
-        // bottom: TabBar(
-        //   tabs: [
-        //     Tab(text: "wk (${wkCount.toString()})"),
-        //     Tab(text: "bat (${batCount.toString()})"),
-        //     Tab(text: "bowl (${bowlCount.toString()})"),
-        //     Tab(text: "AR (${arCount.toString()})"),
-        //   ],
-        // ),
-      ),
-      body: ModalProgressHUD(
-        inAsyncCall: isDataLoading,
-        child: Column(
-          children: [
-            Expanded(
-              child: isDataLoading
-                ? Center(
-                    child: CircularProgressIndicator(),
-                  )
-                : DefaultTabController(
-                    length: 4,
-                    child: Column(
-                      children: [
-                       TabBar(
-  labelColor: Colors.black, // Set the text color of the selected (active) tab to black
-  unselectedLabelColor: Colors.grey, // Set the text color of unselected (inactive) tabs to grey or any color you prefer
-  tabs: [
-    Tab(text: "WK (${wkCount.toString()})"),
-    Tab(text: "BAT (${batCount.toString()})"),
-    Tab(text: "BOWL (${bowlCount.toString()})"),
-    Tab(text: "AR (${arCount.toString()})"),
-  ],
-),
-                        Expanded(
-                          child: TabBarView(
+                Center(
+                  child: Text(header, style: TextStyle(fontWeight: FontWeight.bold)),
+                ),
+                DataTable(
+                  columns: [
+                    DataColumn(label: Text('Players')),
+                    DataColumn(label: Text('Points')),
+                    DataColumn(label: Text('Credits')),
+                    DataColumn(label: Text('Select')),
+                  ],
+                  dataRowHeight: 60,
+                  columnSpacing: 10,
+                  rows: filteredPlayers.map((player) {
+                    final logoUrl = player['logo_url'] as String?;
+                    final fantasyPlayerRating = player['fantasy_player_rating'];
+                    final isPlayerSelected = selectedPlayers.contains(player);
+
+                    return DataRow(
+                      selected: isPlayerSelected, // Set the selected property based on whether the player is selected
+                      cells: [
+                        DataCell(
+                          Row(
                             children: [
-                              playerListView("wk"),
-                              playerListView("bat"),
-                              playerListView("bowl"),
-                              playerListView("all"),
+                              Flexible(
+                                child: Text(
+                                  player['short_name'] ?? '',
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                              SizedBox(height: 10, width: 10),
+                              Text(
+                                playerPlayingStatus[player['short_name']] == true ? "Playing" : "Not Playing",
+                                style: TextStyle(
+                                  color: playerPlayingStatus[player['short_name']] == true
+                                      ? Colors.green
+                                      : Colors.red,
+                                ),
+                              ),
                             ],
                           ),
                         ),
+                        DataCell(Text("0.0")),
+                        DataCell(Text("$fantasyPlayerRating")),
+                        // The "+" button for player selection goes here
+                        DataCell(
+                          IconButton(
+                            icon: Icon(isPlayerSelected ? Icons.remove : Icons.add),
+                            onPressed: () {
+                              togglePlayerSelection(player);
+                            },
+                          ),
+                        ),
                       ],
-                    ),
-                  ),
-            ),
-            if (selectedPlayers.length == 11)
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => PreviewScreen(selectedPlayers: selectedPlayers),
-                    ),
-                  );
-                },
-                child: Text("Preview"),
-              ),
-          ],
-        ),
-      ),
-    ),
-  );
-}
-
-  Widget playerListView(String role) {
-  List<Map<String, dynamic>> filteredPlayers = allPlayerList.where((player) {
-    return (player['playing_role'] ?? '').toLowerCase() == role.toLowerCase();
-  }).toList();
-
-  return filteredPlayers.isEmpty
-    ? Center(
-        child: Text("No $role players available."),
-      )
-    : ListView(
-      shrinkWrap: true, // Add this line
-      children: filteredPlayers.map((player) {
-        final logoUrl = player['logo_url'] as String?;
-        final fantasyPlayerRating = player['fantasy_player_rating'];
-        final isPlaying = playing11.contains(player);
-        final isPlayerSelected = selectedPlayers.contains(player);
-        final playingStatus = isPlaying ? "Playing 11" : "Not Playing";
-        final textColor = isPlaying ? Colors.green : Colors.red;
-        final selectButtonText = isPlayerSelected ? "Deselect" : "Select";
-
-        return Card(
-          margin: EdgeInsets.all(8.0),
-          color: isPlayerSelected ? Colors.blue : isPlaying ? Colors.green : Colors.white,
-          child: ListTile(
-            title: Row(
-              children: [
-                Text(player['short_name'] ?? ''),
-                SizedBox(width: 15),
-                Text("${player['nationality'] ?? ''}"),
-                SizedBox(width: 15),
-                Text("$fantasyPlayerRating"),
-              ],
-            ),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  playingStatus,
-                  style: TextStyle(color: textColor),
+                    );
+                  }).toList(),
                 ),
               ],
             ),
-            trailing: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    togglePlayerSelection(player);
-                  },
-                  child: Text(selectButtonText),
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(
-                      isPlayerSelected ? Colors.red : Colors.green,
-                    ),
-                  ),
-                ),
-                // if (logoUrl != null)
-                //   Image.network(
-                //     logoUrl,
-                //     logoUrl,
-                //     height: 50,
-                //     width: 50,
-                //   ),
-              ],
-            ),
-          ),
-        );
-      }).toList(),
-    );
-}
+          );
+  }
 }
 
 class PreviewScreen extends StatelessWidget {
@@ -344,7 +371,7 @@ class PreviewScreen extends StatelessWidget {
         children: selectedPlayers.map((player) {
           return ListTile(
             title: Text(player['short_name'] ?? ''),
-            subtitle: Text(player['nationality'] ?? ''),
+            subtitle: Text(player['nationality']?.toUpperCase() ?? ''),
             trailing: Image.network(
               player['logo_url'] ?? '',
               height: 50,
