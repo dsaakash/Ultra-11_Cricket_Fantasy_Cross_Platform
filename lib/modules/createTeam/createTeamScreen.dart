7e2b4dd51aece1c4858da1076825d11c9b6c4329
Aakash <99490497+dsaakash@users.noreply.github.com>
2023-11-10 17:33:30 +0530

    added

--- lib/modules/createTeam/createTeamScreen.dart
+++ lib/modules/createTeam/createTeamScreen.dart
@@ -3,17 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: CreateTeamScreen(),
-    );
-  }
-}
-
 class CreateTeamScreen extends StatefulWidget {
   @override
   _CreateTeamScreenState createState() => _CreateTeamScreenState();
@@ -42,6 +31,28 @@ class _CreateTeamScreenState extends State<CreateTeamScreen> {
   void initState() {
     super.initState();
     fetchData();
+  }
+
+  Widget _buildPlayerAvatar(Map<String, dynamic> player) {
+    final logoUrl = player['logo_url'] as String?;
+    return Column(
+      children: [
+        CircleAvatar(
+          backgroundImage: logoUrl != null
+              ? NetworkImage(logoUrl)
+              : AssetImage('assets/playerImage.png') as ImageProvider,
+          radius: 25,
+        ),
+        SizedBox(height: 4),
+        Text(
+          playerPlayingStatus[player['short_name']] == true ? "Playing" : "Not Playing",
+          style: TextStyle(
+            color: playerPlayingStatus[player['short_name']] == true ? Colors.green : Colors.red,
+            fontSize: 12,
+          ),
+        ),
+      ],
+    );
   }
 
   Future<void> fetchData() async {
@@ -80,7 +91,6 @@ class _CreateTeamScreenState extends State<CreateTeamScreen> {
           setState(() {
             playing11 = playing11Response['players'] ?? [];
 
-            // Update playing status for each player
             for (var player in allPlayerList) {
               final isPlaying = playing11.contains(player);
               playerPlayingStatus[player['short_name']] = isPlaying;
@@ -216,36 +226,36 @@ class _CreateTeamScreenState extends State<CreateTeamScreen> {
             children: [
               Expanded(
                 child: isDataLoading
-                  ? Center(
-                      child: CircularProgressIndicator(),
-                    )
-                  : DefaultTabController(
-                      length: 4,
-                      child: Column(
-                        children: [
-                          TabBar(
-                            labelColor: Colors.black,
-                            unselectedLabelColor: Colors.grey,
-                            tabs: [
-                              Tab(text: "WK (${wkCount.toString()})"),
-                              Tab(text: "BAT (${batCount.toString()})"),
-                              Tab(text: "BOWL (${bowlCount.toString()})"),
-                              Tab(text: "AR (${arCount.toString()})"),
-                            ],
-                          ),
-                          Expanded(
-                            child: TabBarView(
-                              children: [
-                                playerListView("wk", "Pick 1 Wicket-keeper"),
-                                playerListView("bat", "Pick 3-5 Batsmen"),
-                                playerListView("bowl", "Pick 3-5 Bowlers"),
-                                playerListView("all", "Pick 1-3 All-rounders"),
+                    ? Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : DefaultTabController(
+                        length: 4,
+                        child: Column(
+                          children: [
+                            TabBar(
+                              labelColor: Colors.black,
+                              unselectedLabelColor: Colors.grey,
+                              tabs: [
+                                Tab(text: "WK (${wkCount.toString()})"),
+                                Tab(text: "BAT (${batCount.toString()})"),
+                                Tab(text: "BOWL (${bowlCount.toString()})"),
+                                Tab(text: "AR (${arCount.toString()})"),
                               ],
                             ),
-                          ),
-                        ],
+                            Expanded(
+                              child: TabBarView(
+                                children: [
+                                  playerListView("wk", "Wicket-keepers (Pick 1)"),
+                                  playerListView("bat", "Batsmen (Pick 3-5)"),
+                                  playerListView("bowl", "Bowlers (Pick 3-5)"),
+                                  playerListView("all", "All-rounders (Pick 1-3)"),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
               ),
               if (selectedPlayers.length == 11)
                 ElevatedButton(
@@ -268,9 +278,9 @@ class _CreateTeamScreenState extends State<CreateTeamScreen> {
                 style: ButtonStyle(
                   backgroundColor: MaterialStateProperty.resolveWith<Color>((states) {
                     if (selectedPlayers.length == 11) {
-                      return Colors.blue; // Enable when 11 players are selected
+                      return Colors.blue;
                     }
-                    return Colors.grey; // Disable when not all players are selected
+                    return Colors.grey;
                   }),
                 ),
               ),
@@ -290,68 +300,70 @@ class _CreateTeamScreenState extends State<CreateTeamScreen> {
         ? Center(
             child: Text("No $role players available."),
           )
-        : Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: Text(header, style: TextStyle(fontWeight: FontWeight.bold)),
+        : Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Card(
+                color: Colors.grey[300], // Light Grey Background
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Center(
+                    child: Text(
+                      header,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
                 ),
-                DataTable(
-                  columns: [
-                    DataColumn(label: Text('Players')),
-                    DataColumn(label: Text('Points')),
-                    DataColumn(label: Text('Credits')),
-                    DataColumn(label: Text('Select')),
-                  ],
-                  dataRowHeight: 60,
-                  columnSpacing: 10,
-                  rows: filteredPlayers.map((player) {
-                    final logoUrl = player['logo_url'] as String?;
-                    final fantasyPlayerRating = player['fantasy_player_rating'];
-                    final isPlayerSelected = selectedPlayers.contains(player);
+              ),
+              SingleChildScrollView(
+             child: DataTable(
+                columns: [
+                  DataColumn(label: Text('Player')),
+                  DataColumn(label: Text('Points')),
+                  DataColumn(label: Text('Credits')),
+                  DataColumn(label: Text('Select')),
+                ],
+                dataRowHeight: 80,
+                columnSpacing: 10,
+                rows: filteredPlayers.map((player) {
+                  final fantasyPlayerRating = player['fantasy_player_rating'];
+                  final isPlayerSelected = selectedPlayers.contains(player);
 
-                    return DataRow(
-                      selected: isPlayerSelected, // Set the selected property based on whether the player is selected
-                      cells: [
-                        DataCell(
-                          Row(
-                            children: [
-                              Flexible(
-                                child: Text(
-                                  player['short_name'] ?? '',
-                                  overflow: TextOverflow.ellipsis,
-                                ),
+                  return DataRow(
+                    selected: isPlayerSelected,
+                    cells: [
+                      DataCell(
+                        Row(
+                          children: [
+                            _buildPlayerAvatar(player),
+                            SizedBox(width: 10),
+                            Flexible(
+                              child: Text(
+                                player['short_name'] ?? '',
+                                overflow: TextOverflow.ellipsis,
                               ),
-                              SizedBox(height: 10, width: 10),
-                              Text(
-                                playerPlayingStatus[player['short_name']] == true ? "Playing" : "Not Playing",
-                                style: TextStyle(
-                                  color: playerPlayingStatus[player['short_name']] == true
-                                      ? Colors.green
-                                      : Colors.red,
-                                ),
-                              ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
-                        DataCell(Text("0.0")),
-                        DataCell(Text("$fantasyPlayerRating")),
-                        // The "+" button for player selection goes here
-                        DataCell(
-                          IconButton(
-                            icon: Icon(isPlayerSelected ? Icons.remove : Icons.add),
-                            onPressed: () {
-                              togglePlayerSelection(player);
-                            },
-                          ),
+                      ),
+                      DataCell(Text("0.0")),
+                      DataCell(Text("$fantasyPlayerRating")),
+                      DataCell(
+                        IconButton(
+                          icon: Icon(isPlayerSelected ? Icons.remove : Icons.add),
+                          onPressed: () {
+                            togglePlayerSelection(player);
+                          },
                         ),
-                      ],
-                    );
-                  }).toList(),
-                ),
-              ],
-            ),
+                      ),
+                    ],
+                  );
+                }).toList(),
+              )),
+            ],
           );
   }
 }
