45301049732e4f6fc6b7ba08dc3b957d0d9fb5f1
Aakash <99490497+dsaakash@users.noreply.github.com>
2023-11-14 17:45:31 +0530

    Added   validation of each category of player in ui

--- lib/modules/createTeam/createTeamScreen.dart
+++ lib/modules/createTeam/createTeamScreen.dart
@@ -33,42 +33,35 @@ class _CreateTeamScreenState extends State<CreateTeamScreen> {
     fetchData();
   }
 
-Widget _buildPlayerAvatar(Map<String, dynamic> player) {
-  final logoUrl = player['logo_url'] as String?;
-  final defaultImage = AssetImage('assets/playerImage.png');
+  Widget _buildPlayerAvatar(Map<String, dynamic> player) {
+    final logoUrl = player['logo_url'] as String?;
+    final defaultImage = AssetImage('assets/playerImage.png');
 
-  return Column(
-    children: [
-      CircleAvatar(
-        radius: 25,
-        backgroundColor: Colors.transparent,
-        child: logoUrl != null
-            ? Image.network(
-                logoUrl,
-                errorBuilder: (BuildContext context, Object error, StackTrace? stackTrace) {
-                  return Image(image: defaultImage);
-                },
-              )
-            : Image(image: defaultImage),
-      ),
-      SizedBox(height: 4),
-      Text(
-        playerPlayingStatus[player['short_name']] == true ? "Playing" : "Not Playing",
-        style: TextStyle(
-          color: playerPlayingStatus[player['short_name']] == true ? Colors.green : Colors.red,
-          fontSize: 12,
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 25,
+          backgroundColor: Colors.transparent,
+          child: logoUrl != null
+              ? Image.network(
+                  logoUrl,
+                  errorBuilder: (BuildContext context, Object error, StackTrace? stackTrace) {
+                    return Image(image: defaultImage);
+                  },
+                )
+              : Image(image: defaultImage),
         ),
-      ),
-    ],
-  );
-}
-
-
-
-
-
-
-
+        SizedBox(height: 4),
+        Text(
+          playerPlayingStatus[player['short_name']] == true ? "Playing" : "Not Playing",
+          style: TextStyle(
+            color: playerPlayingStatus[player['short_name']] == true ? Colors.green : Colors.red,
+            fontSize: 12,
+          ),
+        ),
+      ],
+    );
+  }
 
   Future<void> fetchData() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -98,7 +91,6 @@ Widget _buildPlayerAvatar(Map<String, dynamic> player) {
           batCount = players.where((player) => (player['playing_role'] ?? '').toLowerCase() == "bat").length;
           arCount = players.where((player) => (player['playing_role'] ?? '').toLowerCase() == "all").length;
           bowlCount = players.where((player) => (player['playing_role'] ?? '').toLowerCase() == "bowl").length;
-          
         });
 
         if (matchId != null) {
@@ -117,10 +109,8 @@ Widget _buildPlayerAvatar(Map<String, dynamic> player) {
         }
       } on DioError catch (e) {
         print("DioException: $e");
-        // Handle DioException, show an error message, or retry the request.
       } catch (e) {
         print("Error fetching data: $e");
-        // Handle other exceptions.
       }
     }
   }
@@ -158,7 +148,6 @@ Widget _buildPlayerAvatar(Map<String, dynamic> player) {
       return players;
     }
   }
-
 
   Future<Map<String, dynamic>?> getMatchDetails(String matchId) async {
     final String query =
@@ -213,110 +202,137 @@ Widget _buildPlayerAvatar(Map<String, dynamic> player) {
       if (selectedPlayers.contains(player)) {
         selectedPlayers.remove(player);
       } else {
-        if (selectedPlayers.length < 11) {
+        if (player['playing_role'] == "wk" && selectedPlayers.where((p) => p['playing_role'] == "wk").length < 2) {
+          selectedPlayers.add(player);
+        } else if (player['playing_role'] == "bat" &&
+            selectedPlayers.where((p) => p['playing_role'] == "bat").length < 5) {
+          selectedPlayers.add(player);
+        } else if (player['playing_role'] == "all" &&
+            selectedPlayers.where((p) => p['playing_role'] == "all").length < 3) {
+          selectedPlayers.add(player);
+        } else if (player['playing_role'] == "bowl" &&
+            selectedPlayers.where((p) => p['playing_role'] == "bowl").length < 5) {
           selectedPlayers.add(player);
         }
+      }
+
+      if (selectedPlayers.length > 11) {
+        // Display a prompt when more than 11 players are selected
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text("Player Limit Exceeded"),
+              content: Text("You can select a maximum of 11 players. Choose your team wisely."),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("OK"),
+                ),
+              ],
+            );
+          },
+        );
       }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 4,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(country1Name, style: TextStyle(fontSize: 14)),
-                  Text(country2Name, style: TextStyle(fontSize: 14)),
-                  Text(time, style: TextStyle(fontSize: 12)),
-                ],
-              ),
-              Image.network(
-                country1Flag,
-                height: 40,
-                width: 40,
-              ),
-              Image.network(
-                country2Flag,
-                height: 40,
-                width: 40,
-              ),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(country1Name, style: TextStyle(fontSize: 14)),
+                Text(country2Name, style: TextStyle(fontSize: 14)),
+                Text(time, style: TextStyle(fontSize: 12)),
+              ],
+            ),
+            Image.network(
+              country1Flag,
+              height: 40,
+              width: 40,
+            ),
+            Image.network(
+              country2Flag,
+              height: 40,
+              width: 40,
+            ),
+          ],
         ),
-        body: ModalProgressHUD(
-          inAsyncCall: isDataLoading,
-          child: Column(
-            children: [
-              Expanded(
-                child: isDataLoading
-                    ? Center(
-                        child: CircularProgressIndicator(),
-                      )
-                    : DefaultTabController(
-                        length: 4,
-                        child: Column(
-                          children: [
-                            TabBar(
-                              labelColor: Colors.black,
-                              unselectedLabelColor: Colors.grey,
-                              tabs: [
-                                Tab(text: "WK (${wkCount.toString()})"),
-                                Tab(text: "BAT (${batCount.toString()})"),
-                                Tab(text: "AR (${arCount.toString()})"),
-                                Tab(text: "BOWL (${bowlCount.toString()})"),
-                                
+      ),
+      body: ModalProgressHUD(
+        inAsyncCall: isDataLoading,
+        child: Column(
+          children: [
+            Expanded(
+              child: isDataLoading
+                  ? Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : DefaultTabController(
+                      length: 4,
+                      child: Column(
+                        children: [
+                          TabBar(
+                            labelColor: Colors.black,
+                            unselectedLabelColor: Colors.grey,
+                            tabs: [
+                              Tab(text: "WK"),
+                              Tab(text: "BAT"),
+                              Tab(text: "AR"),
+                              Tab(text: "BOWL"),
+                            ],
+                          ),
+                          Expanded(
+                            child: TabBarView(
+                              children: [
+                                playerListView("wk", "Wicket-keepers (Pick 2)"),
+                                playerListView("bat", "Batsmen (Pick 3-5)"),
+                                playerListView("all", "All-rounders (Pick 1-3)"),
+                                playerListView("bowl", "Bowlers (Pick 3-5)"),
                               ],
                             ),
-                            Expanded(
-                              child: TabBarView(
-                                children: [
-                                  playerListView("wk", "Wicket-keepers (Pick 1)"),
-                                  playerListView("bat", "Batsmen (Pick 3-5)"),
-                                  playerListView("bowl", "Bowlers (Pick 3-5)"),
-                                  playerListView("all", "All-rounders (Pick 1-3)"),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-              ),
-              if (selectedPlayers.length == 11)
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => PreviewScreen(selectedPlayers: selectedPlayers),
-                      ),
-                    );
-                  },
-                  child: Text("Preview"),
-                ),
-              SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () {
+                    ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => PreviewScreen(selectedPlayers: selectedPlayers),
+                  ),
+                );
+              },
+              child: Text("Preview"),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                if (selectedPlayers.length == 11) {
                   // Continue button logic here
-                },
-                child: Text("Continue"),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.resolveWith<Color>((states) {
-                    if (selectedPlayers.length == 11) {
-                      return Colors.blue;
-                    }
-                    return Colors.grey;
-                  }),
-                ),
+                }
+              },
+              child: Text("Continue"),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith<Color>((states) {
+                  if (selectedPlayers.length == 11) {
+                    return Colors.blue;
+                  }
+                  return Colors.grey;
+                }),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
