ce665475489cbf5b75525c66f61f409cf536dfc0
Aakash <99490497+dsaakash@users.noreply.github.com>
2023-12-11 16:28:09 +0530

    fixed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,20 +7,21 @@ import 'package:tempalteflutter/main.dart';
 //Added
 
 void main() {
-    testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  // Build our app and trigger a frame.
+  await tester.pumpWidget(MyApp());
 
-    // Verify that the initial text is an empty string.
-    expect(find.text(''), findsOneWidget);
+  // Verify that the initial text is '0'.
+  expect(find.text('0'), findsOneWidget);
 
-    // Tap on the widget to trigger the increment.
-    await tester.tap(find.byIcon(Icons.add));
+  // Tap on the widget to trigger the increment.
+  await tester.tap(find.byIcon(Icons.add));
 
-    // Rebuild the widget after the tap.
-    await tester.pump();
+  // Rebuild the widget after the tap.
+  await tester.pump();
 
-    // Verify that the text is now '1' after the increment.
-    expect(find.text('1'), findsOneWidget);
-  });
+  // Verify that the text is now '1' after the increment.
+  expect(find.text('1'), findsOneWidget);
+});
+
 }
