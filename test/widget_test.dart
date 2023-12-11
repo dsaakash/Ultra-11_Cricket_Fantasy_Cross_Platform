9e88d0658af9cd144cbcb749069a6e44b3b9b44a
Aakash <99490497+dsaakash@users.noreply.github.com>
2023-12-11 21:18:34 +0530

    fixed widget_dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,27 +1,27 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tempalteflutter/main.dart';
+// import 'package:tempalteflutter/main.dart';
 
 
-//Added
+// //Added
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-  // Build our app and trigger a frame.
-  await tester.pumpWidget(MyApp());
+// void main() {
+//   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+//   // Build our app and trigger a frame.
+//   await tester.pumpWidget(MyApp());
 
-  // Verify that the initial text is '0'.
-  expect(find.text('0'), findsOneWidget);
+//   // Verify that the initial text is '0'.
+//   expect(find.text('0'), findsOneWidget);
 
-  // Tap on the widget to trigger the increment.
-  await tester.tap(find.byIcon(Icons.add));
+//   // Tap on the widget to trigger the increment.
+//   await tester.tap(find.byIcon(Icons.add));
 
-  // Rebuild the widget after the tap.
-  await tester.pump();
+//   // Rebuild the widget after the tap.
+//   await tester.pump();
 
-  // Verify that the text is now '1' after the increment.
-  expect(find.text('1'), findsOneWidget);
-});
+//   // Verify that the text is now '1' after the increment.
+//   expect(find.text('1'), findsOneWidget);
+// });
 
-}
+// }
